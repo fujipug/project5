0eb2858cb5af68aa78aee9d97facfecab40123aa
Ian Humphrey <irh9@nau.edu>
2015-04-28 10:23:37 -0700

    some comments for adding stuff to datastore

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import webapp2
 from base_handler import BaseHandler
 from posts import PostHandler
 from lots import LotHandler
+from models import ParkingLot
 
 class MainHandler(BaseHandler):
     def get(self):
@@ -13,6 +14,25 @@ class MainHandler(BaseHandler):
         }
 
         self.render("./templates/main.html", template_values)
+
+        # add stuff to datastore here -- or use localhost:8000
+        # -- make sure to comment after adding
+        # lots = [
+        #     ParkingLot(
+        #         name = "P66",
+        #         description = "Skydome",
+        #         ),
+        #     ParkingLot(
+        #         name = "P62",
+        #         description = "South Commuter"
+        #     ),
+        #     ParkingLot(
+        #         name = "P42",
+        #         description = "South of Forestry"
+        #     )
+        # ]
+        # for lot in lots:
+        #     lot.put()
 
 # main application routing
 app = webapp2.WSGIApplication([
