143e07d2c3cb96755d5841e5b97332f97aa5bec4
Lane <lf237@nau.edu>
2015-04-28 21:21:39 -0700

    leave comment button and stuff

--- main.py
+++ main.py
@@ -18,54 +18,54 @@ class MainHandler(BaseHandler):
         # add stuff to datastore here -- or use localhost:8000
         # -- make sure to comment after adding
         lots = [
-        #     models.ParkingLot(
-        #         name = "P13",
-        #         description = "Cline Library"
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P13B",
-        #         description = "Ponderosa"
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P46",
-        #         description = "Forestry/Nursing"
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P62",
-        #         description = "South Commuter"
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P62A",
-        #         description = "South Commuter"
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P62B",
-        #         description = "South Commuter"
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P64",
-        #         description = "Facility Services",
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P65",
-        #         description = "Skydome Side Lot"
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P66",
-        #         description = "Skydome"
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P69A",
-        #         description = "Greenhouse Complex"
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P96A",
-        #         description = "Knoles Parking Garage",
-        #     ),
-        #     models.ParkingLot(
-        #         name = "P96B",
-        #         description = "San Francisco Parking Garage",
-        #     ),
+            # models.ParkingLot(
+            #     name = "P13",
+            #     description = "Cline Library"
+            # ),
+            # models.ParkingLot(
+            #     name = "P13B",
+            #     description = "Ponderosa"
+            # ),
+            # models.ParkingLot(
+            #     name = "P46",
+            #     description = "Forestry/Nursing"
+            # ),
+            # models.ParkingLot(
+            #     name = "P62",
+            #     description = "South Commuter"
+            # ),
+            # models.ParkingLot(
+            #     name = "P62A",
+            #     description = "South Commuter"
+            # ),
+            # models.ParkingLot(
+            #     name = "P62B",
+            #     description = "South Commuter"
+            # ),
+            # models.ParkingLot(
+            #     name = "P64",
+            #     description = "Facility Services",
+            # ),
+            # models.ParkingLot(
+            #     name = "P65",
+            #     description = "Skydome Side Lot"
+            # ),
+            # models.ParkingLot(
+            #     name = "P66",
+            #     description = "Skydome"
+            # ),
+            # models.ParkingLot(
+            #     name = "P69A",
+            #     description = "Greenhouse Complex"
+            # ),
+            # models.ParkingLot(
+            #     name = "P96A",
+            #     description = "Knoles Parking Garage",
+            # ),
+            # models.ParkingLot(
+            #     name = "P96B",
+            #     description = "San Francisco Parking Garage",
+            # ),
         ]
         # for lot in lots:
         #     lot.put()
