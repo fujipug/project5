37adbaaaa9b5625b49d32c0df655a1a47ff8a701
Ian Johnson <itj3@nau.edu>
2015-04-29 11:02:54 -0700

    base_handler checks user + gets account def

--- base_handler.py
+++ base_handler.py
@@ -1,6 +1,7 @@
 import jinja2
 import webapp2
-
+from google.appengine.api import users
+from models import Account
 import os
 
 
@@ -17,6 +18,31 @@ class BaseHandler(webapp2.RequestHandler):
 
     def __init__(self, request=None, response=None):
         super(BaseHandler, self).__init__(request, response)
+        self.user = users.get_current_user()
+        if self.user:
+            self.url = users.create_logout_url('/')
+            self.url_linktext = "Sign out"
+        else:
+            self.url = users.create_login_url(self.request.uri)
+            self.url_linktext = "Sign In"
+    def get_account(self):
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url('/')
+            url_linktext = "Sign out"
+
+            # check if user in db
+            qry = Account.query()
+            acc = []
+            for x in qry:
+                if x.email == user.email():
+                    acc = x
+                    break
+            # if user not in db then add it
+            if not acc:
+                acc = Account(email = user.email())
+                acc.put()
+            return acc
 
     def render(self, template_name, template_values):
         template = jinja_environment.get_template(template_name)
